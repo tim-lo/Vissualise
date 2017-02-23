a2c6d2e4c91ab9bf0c5e11f29bd53629d23453c6
Thomas Li <txl306@bham.ac.uk>
2017-02-23 11:15:22 +0000

    Add redirect URL to authentication link

--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -4,7 +4,7 @@ window.onload = init();
 
 function init() {
   var gh_signin_btn = document.querySelector("#gh-signin-btn");
-  var gh_auth_url = "https://github.com/login/oauth/authorize?scope=user:email repo&client_id=da3d5188a6954fdd74f6";
+  var gh_auth_url = "https://github.com/login/oauth/authorize?scope=user:email repo&client_id=da3d5188a6954fdd74f6&redirect_uri=https://vissualise.herokuapp.com/auth";
   gh_signin_btn.addEventListener("click", function(e) {
     chrome.tabs.create({ url: gh_auth_url });
     console.log("Current tab URL: " + chrome.tabs.Tab.url);
