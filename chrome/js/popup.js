f4cd0220c2cbba0d6dd3a8600c67dd129a47851e
Thomas Li <txl306@bham.ac.uk>
2017-02-23 10:55:30 +0000

    Set up basic server

--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -3,5 +3,10 @@
 window.onload = init();
 
 function init() {
-
+  var gh_signin_btn = document.querySelector("#gh-signin-btn");
+  var gh_auth_url = "https://github.com/login/oauth/authorize?scope=user:email repo&client_id=da3d5188a6954fdd74f6";
+  gh_signin_btn.addEventListener("click", function(e) {
+    chrome.tabs.create({ url: gh_auth_url });
+    console.log("Current tab URL: " + chrome.tabs.Tab.url);
+  });
 }
